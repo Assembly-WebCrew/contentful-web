fc3d762a7f30aa814ba813a63356c0379119d18f
Ville Lahdenvuo <ville.lahdenvuo@grano.fi>
2017-08-05 11:56:47 +0300

    feat: Implement page header, markdown and zipper mock components

--- src/app/content-blocks/block-markdown/block-markdown.component.ts
+++ src/app/content-blocks/block-markdown/block-markdown.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'asm-block-markdown',
   templateUrl: './block-markdown.component.html',
   styleUrls: ['./block-markdown.component.scss']
 })
-export class BlockMarkdownComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class BlockMarkdownComponent {
+  content;
 }
--- src/app/content-blocks/block-zipper/block-zipper.component.ts
+++ src/app/content-blocks/block-zipper/block-zipper.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'asm-block-zipper',
   templateUrl: './block-zipper.component.html',
   styleUrls: ['./block-zipper.component.scss']
 })
-export class BlockZipperComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class BlockZipperComponent {
+  content;
 }
