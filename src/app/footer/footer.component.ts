6365cd34161b2ff01a62118b914935a26864536c
Karolain <karolain@kapsi.fi>
2017-11-04 23:00:09 +0200

    scrolling navigation logic, further fixed header styling, moved images under assets, added footer menu contentful integration

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from '../core/contentful.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import gql from 'graphql-tag';
+import * as qs from 'qs';
+import { get } from 'lodash';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'asm-footer',
@@ -6,10 +12,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  footer$: Observable<any>;
+  event: any;
 
-  constructor() { }
+  constructor(
+    private contentful: ContentfulService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.event = this.route.snapshot.data.event;
+    const params = { 'fields.title': 'Footer Menu' };
+    this.footer$ = this.contentful.query$<any>({
+      query: gql`
+    {
+      menus(q: "${qs.stringify(params)}") {
+        title
+        items {
+          ... on Menu {
+            title
+            items {
+              ... on MenuItem {
+                title
+                url
+                page {
+                  slug
+                }
+              }
+            }
+          }
+        }
+      }
+    }` }).map(data => data.menus[0]);
   }
 
+  getUrl(item) {
+    if (item.page) {
+      return `/${this.event.name}/${item.page.slug}`;
+    } else {
+      if (item.url && item.url[0] === '/')
+        return `/${this.event.name}${item.url}`;
+
+      return item.url;
+    }
+  }
+
+  onNavigation(item, event : Event) {
+    let url : string = this.getUrl(item);
+
+    if (url.startsWith("/")) {
+      event.preventDefault();
+      this.router.navigate([url]);
+      return false;
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,31 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import gql from 'graphql-tag';
 import { ContentfulService } from '../core/contentful.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import * as qs from 'qs';
 import { get } from 'lodash';
 import { Observable } from 'rxjs/Observable';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'asm-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  host: {
+    "(window.scroll)": "onScrollEvent($event)"
+  }
 })
 export class HeaderComponent implements OnInit {
   header$: Observable<any>;
   event: any;
   highlightMenu: any[];
   menu: any[];
+  scrolling: boolean = false;
 
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private contentful: ContentfulService,
+    private router: Router,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -43,6 +50,14 @@ export class HeaderComponent implements OnInit {
     }` }).map(data => data.menus[0]);
   }
 
+  getLogo() {
+    if (this.scrolling) {
+      return "/assets/images/generic-event-logo.png";
+    } else if (this.event && this.event.logo && this.event.logo.fields) {
+      return this.event.logo.fields.file.url;
+    }
+  }
+
   getMenuItems(items, isHighlight: boolean) {
     if (!items)
       return [];
@@ -60,5 +75,29 @@ export class HeaderComponent implements OnInit {
 
       return item.url;
     }
+  }
+
+  onNavigation(item, event : Event) {
+    let url : string = this.getUrl(item);
+
+    if (url.startsWith("/")) {
+      event.preventDefault();
+      this.router.navigate([url]);
+      return false;
+    }
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScrollEvent() {
+    let number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 100) {
+      this.scrolling = true;
+    } else if (this.scrolling && number < 10) {
+      this.scrolling = false;
+    }
+  }
+
+  onScrollEvent(event) {
+
   }
 }
