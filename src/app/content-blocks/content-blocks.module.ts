d61d2fa7c276fc00d5707a51766b2dbd7cc35367
Aurora Tulilaulu <tulilaulu@gmail.com>
2018-02-02 17:50:04 +0200

    new zipper, css missing

--- src/app/content-blocks/content-blocks.module.ts
+++ src/app/content-blocks/content-blocks.module.ts
@@ -1,6 +1,7 @@
 import { ContentBlockComponent } from '../content-block/content-block.component';
 import { CoreModule } from '../core/core.module';
 import { BlockZipperComponent } from './block-zipper/block-zipper.component';
+import { BlockZipperContentComponent } from './block-zipper-content/block-zipper-content.component';
 import { BlockPageHeaderComponent } from './block-page-header/block-page-header.component';
 import { BlockMarkdownComponent } from './block-markdown/block-markdown.component';
 import { NgModule } from '@angular/core';
@@ -15,6 +16,7 @@ export const contentComponents = [
   BlockMarkdownComponent,
   BlockPageHeaderComponent,
   BlockZipperComponent,
+  BlockZipperContentComponent,
   BlockSectionComponent,
   BlockSponsorsComponent
 ];
