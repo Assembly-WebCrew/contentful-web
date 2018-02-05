62151a2ddc1f832248067140f7b2aa43891526a3
Karolain <karolain@kapsi.fi>
2018-02-05 18:29:38 +0200

    fix test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,12 @@
 import { ContentfulWebPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('contentful-web App', () => {
   let page: ContentfulWebPage;
 
   beforeEach(() => {
     page = new ContentfulWebPage();
+    browser.waitForAngularEnabled(false);
   });
 
   it('should contain ASSEMBLY logo', () => {
