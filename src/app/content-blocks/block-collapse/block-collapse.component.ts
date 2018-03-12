cd874d989ef53abc648fcc327e96c1118a25fbba
Aurora Tulilaulu <tulilaulu@gmail.com>
2018-03-12 23:52:30 +0200

    collapse

--- src/app/content-blocks/block-collapse/block-collapse.component.ts
+++ src/app/content-blocks/block-collapse/block-collapse.component.ts
@@ -8,12 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class BlockCollapseComponent implements OnInit{
   content: any = {};
   ngOnInit() {
-    this.event = this.route.snapshot.parent ? this.route.snapshot.parent.data.event : null;
-    if (this.event) {
-      this.startTime = new Date(this.event.startDate).getTime();
-      this.endTime = new Date(this.event.endDate).getTime();
-      this.setCountdown();
-    }
 
   }
 }
