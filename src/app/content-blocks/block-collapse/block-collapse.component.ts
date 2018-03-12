e6758ddb072b856dc1922392fc84de6e5a59fe79
Aurora Tulilaulu <tulilaulu@gmail.com>
2018-03-13 00:12:50 +0200

    now collapse works

--- src/app/content-blocks/block-collapse/block-collapse.component.ts
+++ src/app/content-blocks/block-collapse/block-collapse.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'asm-block-collapse',
   templateUrl: './block-collapse.component.html',
   styleUrls: ['./block-collapse.component.scss']
+
 })
 export class BlockCollapseComponent implements OnInit{
   content: any = {};
+  opened: Boolean = false;
   ngOnInit() {
-
   }
+  toggle(){
+      this.opened = !this.opened;
+  }
+
 }
