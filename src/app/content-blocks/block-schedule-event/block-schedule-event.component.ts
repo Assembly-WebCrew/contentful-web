c3d7664f8be921fec86e36dcfb8cfff05b002b6c
rubiesonthesky <linneawp@gmail.com>
2018-06-22 13:01:28 +0300

    schedulespagetti

--- src/app/content-blocks/block-schedule-event/block-schedule-event.component.ts
+++ src/app/content-blocks/block-schedule-event/block-schedule-event.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'asm-block-schedule-event',
   templateUrl: './block-schedule-event.component.html',
-  styleUrls: ['./block-schedule-event.component.scss']
+  styleUrls: ['./block-schedule-event.component.scss'],
 })
 export class BlockScheduleEventComponent implements OnInit {
   static blockName = 'BlockScheduleEvent';
@@ -13,29 +13,40 @@ export class BlockScheduleEventComponent implements OnInit {
   id: string;
 
   ngOnInit() {
+    this.event.duration = this.getDuration(
+      this.event.start_time,
+      this.event.end_time
+    );
+    this.event.isRescheduled =
+    this.event.start_time !== this.event.original_start_time || false;
+    this.event.isMajor = this.event.flags.includes('major');
+    this.event.icon = this.getCategoryIcon();
   }
 
   toggle() {
-      this.opened = !this.opened;
+    this.opened = !this.opened;
   }
 
-  getDuration(start, end) {
+  getDuration(start: Date, end: Date) {
     const sec = 1000;
     const min = 60 * sec;
     const hour = 60 * min;
     const day = 24 * hour;
     let duration = '';
 
-    const distance  = new Date(end).getTime() - new Date(start).getTime();
+    const distance = new Date(end).getTime() - new Date(start).getTime();
+    // how many days
     const days = Math.floor(distance / hour);
     if (days !== 0) {
       duration = days + ' days';
     }
+    // how many hours after days
     const hours = Math.floor((distance % day) / hour);
     if (hours !== 0) {
       if (days !== 0) duration = duration + ' ';
       duration = duration + hours + ' h';
     }
+    // how many minutes after hours
     const minutes = Math.floor((distance % hour) / min);
     if (minutes !== 0) {
       if (hours !== 0) {
@@ -52,4 +63,31 @@ export class BlockScheduleEventComponent implements OnInit {
     }
   }
 
+  getCategoryIcon() {
+  let icon = '';
+  if (this.event.name.includes('doors' || 'entrance')) {
+    return icon = 'fa-home';
+  } else {
+    this.event.categories.map ( x => {
+
+      const y = x.toString().toLowerCase();
+      console.log(y);
+      if (y === 'game') {
+        console.log('GAME TIME');
+        return icon = 'fa-gamepad';
+      } else if (y === 'streamcorner') {
+        return icon = 'fa-play-circle-o';
+      } else if ((y === 'Demoscene'))  {
+        return icon = 'fa-image';
+      } else if (this.event.name.includes('doors') || this.event.name.includes('entrance') ) {
+        return icon = 'fa-home';
+      }
+    });
+  }
+    return icon;
+  }
+
+  hasCategory(cat) {
+    return;
+  }
 }
--- src/app/content-blocks/block-schedule/block-schedule.component.ts
+++ src/app/content-blocks/block-schedule/block-schedule.component.ts
@@ -4,67 +4,84 @@ import { ScheduleService, Schedule } from '../services/schedule.service';
 @Component({
   selector: 'asm-block-schedule',
   templateUrl: './block-schedule.component.html',
-  styleUrls: ['./block-schedule.component.scss']
+  styleUrls: ['./block-schedule.component.scss'],
 })
-
 export class BlockScheduleComponent implements OnInit {
   static blockName = 'BlockSchedule';
 
   content: any = {};
-  days = [];
-  // calEvents = {};
+  days = {
+    thursday: [],
+    friday: [],
+    saturday: [],
+    sunday: [],
+  };
   times = [];
   schedule: any = {};
-  loading = true;
+  // loading = true;
   events = [];
 
-  constructor(
-    private scheduleService: ScheduleService
-  ) { }
+  constructor(private scheduleService: ScheduleService) {}
 
   ngOnInit() {
-    this.days = ['Thursday', 'Friday', 'Saturday', 'Sunday'];
     this.times = ['12:00', '13:00'];
 
-    this.scheduleService.getJSON('assets/winter18.json')
+    this.scheduleService
+      .getJSON('assets/winter18.json')
       .subscribe((data: Schedule) => {
-      this.schedule = {
-        locations: data.locations,
-        events:  data.events
-      };
-      this.loading = false;
-      console.log('getSchedule');
-      console.log(this.schedule);
-      this.events = this.schedule.events;
-      console.log(this.schedule.events);
-      console.log(this.schedule.locations);
-    });
-
-    console.log(this.events);
-    console.log(this.schedule);
+        this.schedule = {
+          locations: data.locations,
+          events: data.events,
+        };
+        this.events = this.schedule.events;
+        // this.loading = false;
+        this.events.forEach(x => {
+          switch (new Date(x.start_time).getDay()) {
+            case 4:
+              this.days.thursday.push(x);
+              break;
+            case 5:
+              this.days.friday.push(x);
+              break;
+            case 6:
+              this.days.saturday.push(x);
+              break;
+            case 0:
+              this.days.sunday.push(x);
+              break;
+          }
+        });
+      });
   }
 
+  // }` }).then(response => {
+  //   response.data.sponsors.forEach(sponsor => {
+  //     if (sponsor.isMainSponsor) {
+  //       this.mainPartners.push(sponsor);
+  //     } else {
+  //       this.otherPartners.push(sponsor);
+  //     }
+  //   });
 
+  // eventsToDays(events) {
+  //   let i = 0;
+  //   while (events[i]) {
 
-  eventsToDays(events) {
-    let i = 0;
-    while (events[i]) {
-
-      i++;
-    }
-  }
+  //     i++;
+  //   }
+  // }
 
   getDayOfWeek(time) {
     const day = new Date(time).getDay();
     return day;
   }
 
-  checkDay(time, day) {
-    // const date = new Date();
-    const time_temp = new Date(time).getDay();
-    // const day_temp = new Date(day).getDay();
-    const isSame = (time_temp === day);
-    console.log(isSame, time_temp, day);
-    return isSame;
-  }
+  // checkDay(time, day) {
+  //   // const date = new Date();
+  //   const time_temp = new Date(time).getDay();
+  //   // const day_temp = new Date(day).getDay();
+  //   const isSame = (time_temp === day);
+  //   console.log(isSame, time_temp, day);
+  //   return isSame;
+  // }
 }
