af7d9c5fd9b6caae920c3407e88b6c8b4fb6c5a5
Karolain <karolain@kapsi.fi>
2020-09-02 18:02:01 +0300

    WEB-76: Add filtering for the grid

--- src/app/content-blocks/block-grid/block-grid.component.ts
+++ src/app/content-blocks/block-grid/block-grid.component.ts
@@ -11,13 +11,24 @@ export class BlockGridComponent implements OnInit {
   static blockName = 'BlockGrid';
 
   content: any = {};
-  filters = [];
+  filters: string[] = [];
+  activeFilters = new Set<string>();
+  pages = [];
 
   constructor(
     private contentful: ContentfulService) { }
 
   ngOnInit(): void {
-    // TODO: implement filters using tags
+    this.pages = this.content.pages.slice();
+    const filters = [];
+    if (this.pages && this.pages.length) {
+      this.pages.forEach(page => {
+        if (page.tags && page.tags.length) {
+          filters.push(...page.tags.map(tag => tag.title));
+        }
+      });
+    }
+    this.filters = Array.from(new Set(filters).values());
   }
 
   getUrl(slug?: string): string {
@@ -26,5 +37,16 @@ export class BlockGridComponent implements OnInit {
     } else {
       return '';
     }
+  }
+
+  onToggleFilter(filter: string) {
+    if (this.activeFilters.has(filter)) {
+      this.activeFilters.delete(filter);
+    } else {
+      this.activeFilters.add(filter);
+    }
+    this.pages = this.content.pages.filter(page => {
+      return this.activeFilters.size === 0 || (page.tags && page.tags.some(tag => this.activeFilters.has(tag.title)));
+    });
   }
 }
