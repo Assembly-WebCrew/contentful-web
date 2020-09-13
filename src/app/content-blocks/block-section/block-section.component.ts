c075e563534778984c574916e3d2a4dd891e4297
Karolain <karolain@kapsi.fi>
2020-09-13 17:17:36 +0300

    add ids to sections

--- src/app/content-blocks/block-section/block-section.component.ts
+++ src/app/content-blocks/block-section/block-section.component.ts
@@ -9,6 +9,7 @@ export class BlockSectionComponent implements OnInit {
   static blockName = 'BlockSection';
 
   content: any = {};
+  id: string;
   @HostBinding('class.has-background') hasBackground: boolean;
 
   constructor() { }
@@ -16,6 +17,9 @@ export class BlockSectionComponent implements OnInit {
   ngOnInit(): void {
     if (this.content.featuredImage && this.content.featuredImage.url) {
       this.hasBackground = true;
+    }
+    if (this.content.title) {
+      this.id = this.content.title.replace(/\s/gi, '-').replace(/[^\w-]/gi, '').toLowerCase();
     }
   }
 
